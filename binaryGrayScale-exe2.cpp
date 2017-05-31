8a60479b04e2cbc408639f30941408675346de6b
Rodolfo <rodolfo.saldanha@hotmail.com>
2017-05-31 15:36:11 -0300

    Both programs are doing the same as before, but now I am not using the existing functions. Instead, I did everything from scratch

--- binaryGrayScale-exe2.cpp
+++ binaryGrayScale-exe2.cpp
@@ -1,35 +1,52 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace cv;
 using namespace std;
 
-int main(){
-    Mat src, src_gray, binary;
-    int const max_BINARY_value = 255;
-    int threshold_value;
+// Binariza a imagem
+Mat binarizar(int threshold_value, Mat image){
     
-    /// Carrega imagem
-    src = imread("/Users/RodolfoSaldanha/Desktop/opencv-3.2.0/OpenCV/OpenCV/teste.png");
+    int width, height, i, j, soma;
+    width = image.size().width;// pega a largura da imagem
+    height = image.size().height;// pega a altura da imagem
     
-    /// Converte para escala de cinza
-    cvtColor( src, src_gray, CV_BGR2GRAY );
-    
-    while(true)
-    {
-        cout << "Valor do limiar de binarização entre 0 e 255\n";
-        cin >> threshold_value; /// Recebe o limiar de binarização
+    // percorre matriz
+    for(i=0; i<height; i++){
         
-        if(threshold_value > 255)
-            threshold_value = 255;
-        else if(threshold_value < 0)
-            threshold_value = 0;
-        
-        threshold( src_gray, binary, threshold_value, max_BINARY_value,THRESH_BINARY);
-        
-        imshow("Binary Gray Scale", binary );
-        waitKey(0);
+        for(j=0; j<width; j++){
+            
+            soma = 0;
+            Vec3b bgrPixel = image.at<Vec3b>(i,j);// pega RGB de um pixel
+            
+            if(bgrPixel.val[0] >= threshold_value)
+                image.at<Vec3b>(i,j).val[0] = 255;
+            else
+                image.at<Vec3b>(i,j).val[0] = 0;
+            
+        }
     }
     
+    return image;
+}
+
+int main(void){
+    
+    Mat image = imread("/Users/RodolfoSaldanha/Desktop/opencv-3.2.0/OpenCV/OpenCV/teste.png");
+    int threshold_value;
+    
+    cvtColor( image, image, CV_BGR2GRAY );
+    
+    cout << "Valor do limiar de binarização entre 0 e 255\n";
+    cin >> threshold_value; /// Recebe o limiar de binarização
+        
+    if(threshold_value > 255)
+        threshold_value = 255;
+    else if(threshold_value < 0)
+        threshold_value = 0;
+        
+    image = binarizar(threshold_value, image);
+    imshow("Binary", image);
+    waitKey(0);
 }
--- histogramEqualization-ex3.cpp
+++ histogramEqualization-ex3.cpp
@@ -1,31 +1,75 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace cv;
 using namespace std;
 
-int main(){
-    Mat src, equalizada;
+void equalizaHistograma(Mat image){
     
-    /// Carrega imagem
-    src = imread("/Users/RodolfoSaldanha/Desktop/opencv-3.2.0/OpenCV/OpenCV/teste.png");
+    int height, width, i, j, posic, histogram[256], equalizedHistogram[256];
+    double pixelsImage;
+    double soma, aux;
+    width = image.size().width;
+    height = image.size().height;
+    pixelsImage = width * height; // num de pixels da img
     
-    if( !src.data )
-    { cout<<"Usage: ./Histogram_Demo <path_to_image>"<<endl;
-        return -1;}
+    // inicializa vetores
+    for(i=0; i<256; i++){
+        histogram[i] = 0;
+        equalizedHistogram[i] = 0;
+    }
     
-    /// Converte para grayscale
-    cvtColor( src, src, CV_BGR2GRAY );
+    // percorre a matriz
+    for(i=0; i<height; i++){
+        
+        for(j=0; j<width; j++){
+            Vec3b pixel = image.at<Vec3b>(i,j);
+            histogram[pixel.val[0]]++;
+        }
+    }
     
-    /// Applica equalização do histograma
-    equalizeHist(src, equalizada);
+    //equalizando o histograma da imagem
+    for(i=0; i<256; i++){
+        
+        soma=0;
+        for(j=0;j<=i;j++){
+            
+            aux = (double)(histogram[j]/pixelsImage);
+            soma=soma+aux;
+        }
+        
+        soma = soma*255;
+        equalizedHistogram[i] = (int)(soma);
+    }
     
-    imshow("Grayscale", src );
-    imshow("Equalizada", equalizada );
+    // atribui o valor do histograma equalizado para a img
+    for(i=0; i<height; i++){
+        
+        for(j=0; j<width; j++){
+            
+            Vec3b pixel = image.at<Vec3b>(i,j);
+            
+            // verifica qual o valor de cada canal do pixel
+            for(posic=0; posic<256; posic++){
+                if(posic == pixel.val[0])
+                    break;
+            }
+            // atribui o valor do histograma equalizado
+            // apenas 1 canal que é usado pela conversão CV_BGR2GRAY
+            image.at<Vec3b>(i,j).val[0] = equalizedHistogram[posic];
+        }
+    }
     
+    // exibe a imagem
+    imshow("Histograma Equalizado", image);
     waitKey(0);
+}
+
+int main(void){
     
-    return 0;
+    Mat imagem = imread("/Users/RodolfoSaldanha/Desktop/opencv-3.2.0/OpenCV/OpenCV/teste.png");
+    cvtColor(imagem, imagem, CV_BGR2GRAY);
+    imshow("Grayscale", imagem);
+    equalizaHistograma(imagem);
 }
